38e70b7ffbbbf0bc62f1ce077f4c6888fd8045c3
gp2593 <gaopeng.pb@gmail.com>
2013-08-20 20:30:08 +0000

    Bootloader Hello World

---
+++ bootloader/src.asm
@@ -0,0 +1,43 @@
+	org 7C00h
+	jmp short Start
+
+Msg:	db "Hello World! "
+EndMsg:
+
+Start:
+	mov bx, 000Fh
+	mov cx, 1
+	xor dx, dx
+	mov ds, dx
+	cld
+
+Print:
+	mov si, Msg
+
+Char:
+	mov ah, 2
+	int 10h
+	lodsb
+
+	mov ah, 9
+	int 10h
+
+	inc dl
+
+	cmp dl, 80
+	jne Skip
+	xor dl, dl
+	inc dh
+
+	cmp dh, 25
+	jne Skip
+	xor dh, dh
+
+Skip:
+	cmp si, EndMsg
+	jne Char
+	hlt
+times 0200h - 2 - ($ - $$) db 0
+	dw 0AA55h
+
+times 1474560 - ($ - $$) db 0
